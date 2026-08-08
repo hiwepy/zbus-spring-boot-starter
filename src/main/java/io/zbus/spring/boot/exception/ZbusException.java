ea9d3bd30d95d336c5f0862a5579b3c863c2a629
wandl-6A72h <hiwepy@gmail.com>
2026-08-09 00:02:59 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 2.3.x branch

--- src/main/java/io/zbus/spring/boot/exception/ZbusException.java
+++ src/main/java/io/zbus/spring/boot/exception/ZbusException.java
@@ -1,19 +1,36 @@
 package io.zbus.spring.boot.exception;
 
+/**
+ * Runtime exception thrown by the Zbus starter when consumer/producer/RPC
+ * initialisation or message processing fails.
+ *
+ * @author [@Loong Wan](https://github.com/loong10k)
+ * @since 1.0.0
+ */
 @SuppressWarnings("serial")
 public class ZbusException extends RuntimeException {
 
+    /**
+     * @param e the underlying exception
+     */
     public ZbusException(Exception e) {
         super(e.getMessage(), null);
     }
-    
+
+    /**
+     * @param errorMessage the error message
+     */
     public ZbusException(String errorMessage) {
         super(errorMessage, null);
     }
-    
+
+    /**
+     * @param errorMessage the error message
+     * @param cause        the underlying cause
+     */
     public ZbusException(String errorMessage, Throwable cause) {
         super(errorMessage, cause);
     }
- 
-    
+
+
 }
